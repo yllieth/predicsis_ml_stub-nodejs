487033a4ac2089930fe725bae28f4abd15107db6
Sylvain <sylvain.ragot@predicsis.com>
2015-03-17 19:47:08 +0100

    add express server and its grunt task

---
+++ Gruntfile.js
@@ -0,0 +1,47 @@
+module.exports = function(grunt) {
+
+  require('load-grunt-tasks')(grunt);
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    express: {
+      identity: {
+        options: {
+          server: 'src/stubbed-idproxy.js',
+          port: 8002,
+          showStack: true,
+          hostname:'0.0.0.0'
+        }
+      },
+      api: {
+        options: {
+          server: 'src/stubbed-api.js',
+          port: 8003,
+          showStack: true,
+          hostname:'0.0.0.0'
+        }
+      },
+      oauth: {
+        options: {
+          server: 'src/stubbed-identity.js',
+          port: 8004,
+          showStack: true,
+          hostname:'0.0.0.0'
+        }
+      },
+      s3: {
+        options: {
+          server: 'src/stubbed-s3.js',
+          port: 8005,
+          showStack: true,
+          hostname:'0.0.0.0'
+        }
+      }
+    }
+  });
+
+  grunt.registerTask('default', ['express', 'express-keepalive']);
+
+};
