7ff9b4ab852b89318949e58287a9615c29c87db3
Sylvain <sylvain.ragot@predicsis.com>
2015-04-21 09:12:15 +0200

    support /settings/applications routes

--- src/routes/api/route_settings.js
+++ src/routes/api/route_settings.js
@@ -6,6 +6,10 @@ var express = require('express');
 var router = express.Router();
 var _ = require('lodash-contrib');
 
+// ---------------------------------------------------------------------------------------------------------------------
+// ===                                                    TOKENS                                                     ===
+// ---------------------------------------------------------------------------------------------------------------------
+
 router.post('/tokens', function(req, res) {
   //var name = _.getPath(req, 'body.token.name');
   res.status(200).json({
@@ -52,6 +56,47 @@ router.get('/tokens/:id', function(req, res) {
 });
 
 router.delete('/tokens/:id', function(req, res) {
+  res.status(204).json();
+});
+
+// ---------------------------------------------------------------------------------------------------------------------
+// ===                                                 APPLICATIONS                                                  ===
+// ---------------------------------------------------------------------------------------------------------------------
+
+var applications = [
+  {
+    id: "540778b5353834003e050000",
+    name: "essai",
+    uid: "829b6d53ac2736cf9670725eb22288dd3914d4d3a811b0d036fd5eb2bc275540",
+    secret: "e7a9d71fbcd0e0f4f1a78b7db8d8311d91517ca0ee1d72594db11b6a60a13732",
+    redirect_uri: "http://truc.bidule",
+    owner_id: "540762703263310008000000",
+    created_at: "2014-09-03T20:23:17.025Z",
+    updated_at: "2014-09-03T20:23:17.025Z"
+  }
+];
+
+router.post('/applications', function(req, res) {
+  //var name = _.getPath(req, 'body.application.name');
+  //var redirectUri = _.getPath(req, 'body.application.redirect_uri');
+  res.status(200).json({ applications: applications[0] });
+});
+
+router.get('/applications', function(req, res) {
+  res.status(200).json({ applications: applications });
+});
+
+router.get('/applications/:id', function(req, res) {
+  res.status(200).json({ application: applications[0] });
+});
+
+router.patch('/applications/:id', function(req, res) {
+  //var name = _.getPath(req, 'body.application.name');
+  //var redirectUri = _.getPath(req, 'body.application.redirect_uri');
+  res.status(200).json({ application: applications[0] });
+});
+
+router.delete('/applications/:id', function(req, res) {
   res.status(204).json();
 });
 
